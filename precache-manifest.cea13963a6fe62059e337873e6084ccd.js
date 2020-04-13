70d71e21c215937a68b4788d2d14acf2fc4bb39a
Akhilesh <akhilesh_k@outlook.com>
2020-04-13 09:29:09 +0530

    Updates

---
+++ precache-manifest.cea13963a6fe62059e337873e6084ccd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b43c117d706cf415bae7fe2ab9e2fa",
+    "url": "/akhilesh-k.github.io/index.html"
+  },
+  {
+    "revision": "3181c340cb9ec70b2fcb",
+    "url": "/akhilesh-k.github.io/static/css/2.ab965100.chunk.css"
+  },
+  {
+    "revision": "733d68458257355a8819",
+    "url": "/akhilesh-k.github.io/static/css/main.e0e001ef.chunk.css"
+  },
+  {
+    "revision": "3181c340cb9ec70b2fcb",
+    "url": "/akhilesh-k.github.io/static/js/2.f00006d1.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/akhilesh-k.github.io/static/js/2.f00006d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733d68458257355a8819",
+    "url": "/akhilesh-k.github.io/static/js/main.d2c5c717.chunk.js"
+  },
+  {
+    "revision": "06efa7f8f11bb3cc4137",
+    "url": "/akhilesh-k.github.io/static/js/runtime-main.e68adc1c.js"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/akhilesh-k.github.io/static/media/NotoSans-Regular.5a87cce8.ttf"
+  },
+  {
+    "revision": "db869de9f39909c57e535b65317e51ef",
+    "url": "/akhilesh-k.github.io/static/media/dev-dot-to.db869de9.svg"
+  },
+  {
+    "revision": "77c43ad214e867d89f1d76eda87638dd",
+    "url": "/akhilesh-k.github.io/static/media/github.77c43ad2.svg"
+  },
+  {
+    "revision": "da6e483b6e2b7092fba9dc66a67e3182",
+    "url": "/akhilesh-k.github.io/static/media/gmail.da6e483b.svg"
+  },
+  {
+    "revision": "d9ff69d93ac4cc6c0b2088f4820b6dc7",
+    "url": "/akhilesh-k.github.io/static/media/instagram.d9ff69d9.svg"
+  },
+  {
+    "revision": "b8f420b1d589d1c72703f8ffb598dcf6",
+    "url": "/akhilesh-k.github.io/static/media/jupyter.b8f420b1.png"
+  },
+  {
+    "revision": "a421fcee7490b8d125ce8072cc725cfe",
+    "url": "/akhilesh-k.github.io/static/media/linkedin.a421fcee.svg"
+  },
+  {
+    "revision": "510a4388ef77fb800cbb4243e332a925",
+    "url": "/akhilesh-k.github.io/static/media/medium.510a4388.svg"
+  },
+  {
+    "revision": "d1e7714bf4a3317a898bccd367eacbc3",
+    "url": "/akhilesh-k.github.io/static/media/profile.d1e7714b.png"
+  },
+  {
+    "revision": "4d400de6c308fae03da2e191b3e64c34",
+    "url": "/akhilesh-k.github.io/static/media/twitter.4d400de6.svg"
+  }
+]);
